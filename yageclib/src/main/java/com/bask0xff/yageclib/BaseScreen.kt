7d784941915d3f8c771ff54f4e695fa32b04eb89
sbaskov <sbaskov@callpanda.com>
2023-04-02 12:29:28 +0400

    add swipes

--- yageclib/src/main/java/com/bask0xff/yageclib/BaseScreen.kt
+++ yageclib/src/main/java/com/bask0xff/yageclib/BaseScreen.kt
@@ -6,6 +6,17 @@ import android.util.Log
 import android.view.MotionEvent
 
 open class BaseScreen(private val name: String, private val gameLogic: GameLogic) : IScreen {
+
+    private var swipeDirection: GameLogic.SwipeDirection = GameLogic.SwipeDirection.NONE
+    var swipeState = 0; // 0 -nothing, 1 - start, 2 - moving, 3 - end
+    var swipeDx = 0
+    var swipeDy = 0
+    var swipeGradient = 0
+    var touchStartX = 0
+    var touchStartY = 0
+    var touchEndX = 0
+    var touchEndY = 0
+
     fun Name(): String {
         return name
     }
@@ -36,9 +47,43 @@ open class BaseScreen(private val name: String, private val gameLogic: GameLogic
     override fun Render(frames: Int, canvas: Canvas?) {}
     override fun Update() {}
     override fun Destroy() {}
-    override fun OnTouchDown(x: Float, y: Float) {}
-    override fun OnTouchUp(x: Float, y: Float) {}
-    override fun OnMove(event: MotionEvent) {}
+    override fun OnTouchDown(x: Float, y: Float) {
+        swipeState = 1
+        touchStartX = x.toInt()
+        touchStartY = y.toInt()
+    }
+    override fun OnTouchUp(x: Float, y: Float) {
+        swipeState = 2
+
+        touchEndX = x.toInt()
+        touchEndY = y.toInt()
+
+        val dx = touchEndX - touchStartX
+        var dy = touchEndY - touchStartY
+        val dxa = Math.abs(dx)
+        var dya = Math.abs(dy)
+
+        if(dxa > 20 || dya > 20)
+        {
+            if(dxa > dya){
+                if(dx > 0)
+                    swipeDirection = GameLogic.SwipeDirection.SWIPE_RIGHT
+                else
+                    swipeDirection = GameLogic.SwipeDirection.SWIPE_LEFT
+            }
+            else{
+                if(dy > 0)
+                    swipeDirection = GameLogic.SwipeDirection.SWIPE_DOWN
+                else
+                    swipeDirection = GameLogic.SwipeDirection.SWIPE_UP
+            }
+        }
+
+        swipeState = 0
+    }
+    override fun OnMove(event: MotionEvent) {
+        swipeState = 2
+    }
 
     companion object {
         private val TAG = BaseScreen::class.java.simpleName
